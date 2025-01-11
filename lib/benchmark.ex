3421f62dbc9b3d6922540130aafd906b73cf6c0a
Julien Smeets <16883019+juulSme@users.noreply.github.com>
2025-01-11 17:42:03 +0100

    Add timed benchmark

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -2,43 +2,30 @@ defmodule Benchmark do
   @moduledoc """
   Simple benchmarking utility, suitable for comparing multi-threaded performance.
   """
+  require Logger
 
   @doc """
   Benchmark a single function.
 
   ## Options
-  - `:processes` number of concurrent processes
-  - `:count` total number of operations to run (ops per task is div(count, processes))
+  - `:tasks` number of concurrent tasks
+  - `:count` total number of operations to run (ops per task is div(count, tasks))
+  - `:bench_for` seconds to bench, estimates rate first
   """
   @spec bench((-> any()), keyword()) :: binary()
-  def bench(fun, opts \\ []) do
-    processes = opts[:processes] || System.schedulers_online()
-    count = opts[:count] || 10_000_000
-    fn _ -> fun.() end |> do_bench(processes, count)
+  def bench(fun, opts) do
+    opts = Map.new(opts)
+    tasks = opts[:tasks] || System.schedulers_online()
+
+    case opts do
+      %{bench_for: _, count: _} -> raise(RuntimeError, "use bench_for OR count")
+      %{count: count} -> runtime(fun, tasks, count) |> to_rate(count) |> format_int()
+      _ -> timed_bench(fun, tasks, opts[:bench_for] || 5) |> format_int()
+    end
   end
 
-  defp do_bench(fun, 1, count) do
-    start = System.monotonic_time(:nanosecond)
-    1..count |> Stream.each(fun) |> Stream.run()
-    stop = System.monotonic_time(:nanosecond)
-    calc_rate(count, start, stop)
-  end
-
-  defp do_bench(fun, tasks, count) do
-    per_task = div(count, tasks)
-    start = System.monotonic_time(:nanosecond)
-
-    1..tasks
-    |> Enum.map(fn _ ->
-      Task.async(fn ->
-        1..per_task |> Stream.each(fun) |> Stream.run()
-      end)
-    end)
-    |> Task.await_many(:infinity)
-
-    stop = System.monotonic_time(:nanosecond)
-    calc_rate(count, start, stop)
-  end
+  def bench(_opts = [fun | opts]), do: bench(fun, opts)
+  def bench(fun), do: bench(fun, [])
 
   @doc """
   Benchmark and compare multiple things. Expects an enum of pairs of a name and a list of arguments for `bench/1`.
@@ -47,12 +34,40 @@ defmodule Benchmark do
   def bench_many(pairs) do
     pairs
     |> Enum.map(fn {name, bench_args} ->
+      name = to_string(name)
       IO.write("Running #{name}... ")
-      {name, apply(__MODULE__, :bench, bench_args)} |> tap(fn _ -> IO.puts("done.") end)
+
+      {name, apply(__MODULE__, :bench, [bench_args])}
+      |> tap(fn {_k, v} -> IO.puts("final #{v} ops/s, done.") end)
     end)
     |> tap(fn _ -> IO.puts("") end)
     |> format_results()
   end
+
+  defp runtime(fun, 1, count) do
+    start = System.monotonic_time(:nanosecond)
+    repeat(fun, count)
+    stop = System.monotonic_time(:nanosecond)
+    stop - start
+  end
+
+  defp runtime(fun, tasks, count) do
+    per_task = div(count, tasks)
+
+    1..tasks
+    |> Enum.map(fn _ -> Task.async(fn -> runtime(fun, 1, per_task) end) end)
+    |> Task.await_many(:infinity)
+    # max seems better then average; it's not done until it's all done
+    |> Enum.max()
+  end
+
+  defp timed_bench(fun, tasks, target_duration) do
+    estimated_rate = estimate_rate(fun, tasks)
+    target_count = target_duration * estimated_rate
+    runtime(fun, tasks, target_count) |> to_rate(target_count)
+  end
+
+  defp to_rate(duration, count), do: floor(count / duration * 1_000_000_000)
 
   defp format_results(results) do
     name_length = Enum.reduce(results, 0, fn {name, _}, acc -> max(acc, String.length(name)) end)
@@ -67,20 +82,33 @@ defmodule Benchmark do
     |> Enum.join("\n")
   end
 
-  defp calc_rate(count, start, stop) do
-    duration = stop - start
-    floor(count / duration * 1_000_000_000) |> format_int()
-  end
-
   defp format_int(int) do
     int
-    |> Integer.to_string()
-    |> String.to_charlist()
+    |> Integer.to_charlist()
     |> Enum.reverse()
     |> Enum.chunk_every(3)
     |> Enum.intersperse(~c"_")
     |> List.flatten()
     |> Enum.reverse()
     |> List.to_string()
+  end
+
+  defp repeat(fun, count)
+  defp repeat(_, 0), do: :done
+
+  defp repeat(fun, count) do
+    fun.()
+    repeat(fun, count - 1)
+  end
+
+  defp estimate_rate(fun, tasks, count \\ 1) do
+    total_count = max(tasks, count)
+    duration = runtime(fun, tasks, total_count)
+
+    if duration > 100_000_000 do
+      to_rate(duration, total_count) |> tap(&IO.write("estimate #{format_int(&1)} ops/s... "))
+    else
+      estimate_rate(fun, tasks, count * 10)
+    end
   end
 end
